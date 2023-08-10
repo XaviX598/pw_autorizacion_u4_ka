e5db8be0b8e41451969cc4bd88fd0b1a5a1753b4
Xavier Aguilar <kevinjkevps4@gmail.com>
2023-08-09 19:47:14 -0500

    first commit

---
+++ src/main/java/com/example/demo/PwAutorizacionU4KaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwAutorizacionU4KaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwAutorizacionU4KaApplication.class, args);
+	}
+
+}
