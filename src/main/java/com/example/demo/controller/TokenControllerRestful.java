3dd62a84b275a1b942894375a3454c06121c40bb
°°PRISCI NENITA°° <kevinjkevps4@gmail.com>
2023-09-02 10:35:44 -0500

    examen

--- src/main/java/com/example/demo/controller/TokenControllerRestful.java
+++ src/main/java/com/example/demo/controller/TokenControllerRestful.java
@@ -6,7 +6,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,11 +21,11 @@ public class TokenControllerRestful {
 
 	@Autowired
 	private AuthenticationManager authenticationManager;
-	
+
 	@Autowired
 	private JwtUtils jwtUtils;
 
-	@GetMapping("/obtener")
+	@PostMapping("/obtener")
 	public String obtenerToken(@RequestBody UsuarioTO usuario) {
 		this.authenticated(usuario.getUsername(), usuario.getPassword());
 		return this.jwtUtils.generateJwtToken(usuario.getUsername());
